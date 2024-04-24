51bf84b8969108ceb78c04d6bdcee8292c08e037
Joshua Dean Küpper <admin@joshua-kuepper.de>
2024-04-24 21:16:12 +0200

    bump version for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 // define the gav coordinates of this project
 group = "net.scrayos"
-version = "0.6.0-SNAPSHOT"
+version = "0.6.0"
 description = "Xenos Client (Kotlin/Java)"
 
 // hook the plugins for the builds
